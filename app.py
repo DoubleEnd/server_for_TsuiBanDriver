cb004f05778010aa0d76ac0182b66eac62c3688d
DoubleEnd <1291024619@qq.com>
2025-02-26 15:32:32 +0800

    更新了获取rss订阅链接的方法

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from flask import Flask, request, jsonify
 from get_info import get_info_list
+from get_rsslink import get_rss_link
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # 允许所有跨域请求
 
 @app.route("/searchAllInfo", methods=["GET", "POST"])
-def submit():
+def submit_info():
     if request.method == "POST":
         data = request.json  # 获取 JSON 数据
-        moviename = data.get("name")  # 从 JSON 中提取 name 字段
-        result = get_info_list(moviename)  # 调用 get_info_list 函数
+        moviename = data.get("name")
+        result = get_info_list(moviename)
+
+        return jsonify({"code": 200, "msg": "success", "data": result})
+
+
+    elif request.method == "GET":
+        return jsonify({"error": "请使用 POST 方法提交数据"}), 400
+
+@app.route("/getRSSLink", methods=["GET", "POST"])
+def submit_rsslink():
+    if request.method == "POST":
+        data = request.json  # 获取 JSON 数据
+        id = data.get("id")
+        result = get_rss_link(id)
 
         return jsonify({"code": 200, "msg": "success", "data": result})
 
